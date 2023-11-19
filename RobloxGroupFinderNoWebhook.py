a902bced8354a075a9db99dd9033993ddd74791d
SquidDog <rodrigobatalloso@proton.me>
2023-11-19 19:32:04 +0100

    Create RobloxGroupFinderNoWebhook.py

---
+++ RobloxGroupFinderNoWebhook.py
@@ -0,0 +1,23 @@
+import json
+import random
+import requests
+while True:
+  ID = random.randint(9450000, 10000000)
+  r = requests.get(f'https://groups.roproxy.com/v1/groups/{ID}')
+  try:
+    group_data = r.json()
+  except json.JSONDecodeError as e:
+    print(f"JSONDecodeError: {e}")
+    continue 
+  if "owner" in r.text:
+    if group_data['owner'] is None: 
+      if group_data['publicEntryAllowed'] is True and 'isLocked' not in r.text:
+        print(f"\x1b[42mUnclaimed > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
+      else:
+        print(f"\x1b[31mLocked > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
+    else:
+      print(f"\x1b[31mClaimed > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
+  elif any(error.get('message') == "Too many requests" for error in group_data.get('errors', [])):
+    print(f"\x1b[31mToo Many Requests > https://www.roblox.com/groups/{ID}")
+  else:
+    print(f"\x1b[31mInvalid > https://www.roblox.com/groups/{ID}")
