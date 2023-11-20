a3628fc6101b073b98ddc8007d4ac09956b2ca7c
SquidDog <rodrigobatalloso@proton.me>
2023-11-20 21:57:00 +0100

    Update RobloxGroupFinder.py

--- RobloxGroupFinder.py
+++ RobloxGroupFinder.py
@@ -5,7 +5,7 @@ from discord_webhook import DiscordEmbed, DiscordWebhook
 while True:
   ID = random.randint(9450000, 10000000)
   webhook = DiscordWebhook(url="YOUR_WEBHOOK_HERE")
-  r = requests.get(f'https://groups.roproxy.com/v1/groups/{ID}')
+  r = requests.get(f"https://groups.roproxy.com/v1/groups/{ID}")
   try:
     group_data = r.json()
   except json.JSONDecodeError as e:
@@ -13,20 +13,22 @@ while True:
     continue 
   if "owner" in r.text:
     if group_data['owner'] is None: 
-      if group_data['publicEntryAllowed'] is True and 'isLocked' not in r.text:
-        print(f"\x1b[42mUnclaimed > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
-        embed = DiscordEmbed(title='Unclaimed Group Found!', color=000000)
-        embed.add_embed_field(name='ID', value=f"{ID}")
-        embed.add_embed_field(name='Description', value=f"{group_data['description']}")
-        embed.add_embed_field(name='Members', value=f"{group_data['memberCount']}")
-        embed.add_embed_field(name='Link', value=f'https://www.roblox.com/groups/{ID}')
+      if group_data['publicEntryAllowed'] is True and "isLocked" not in r.text:
+        print(f"\x1b[32mUnclaimed | https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
+        embed = DiscordEmbed(title=f"Unclaimed Group | {group_data['name']}", description=group_data['description'], color=0xff0000)
+        embed.add_embed_field(name="Shout", value=f"{group_data['shout']}")
+        embed.add_embed_field(name="Members", value=f"{group_data['memberCount']}")
+        embed.add_embed_field(name="Entry Allowed", value=f"{group_data['publicEntryAllowed']}")
+        embed.add_embed_field(name="Verified", value=f"{group_data['hasVerifiedBadge']}")
+        embed.add_embed_field(name="ID", value=f"{ID}")
+        embed.add_embed_field(name="Link", value=f"https://www.roblox.com/groups/{ID}")
         webhook.add_embed(embed)
         response = webhook.execute()
       else:
-        print(f"\x1b[31mLocked > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
+        print(f"\x1b[33mLocked | https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
     else:
-      print(f"\x1b[31mClaimed > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
-  elif any(error.get('message') == "Too many requests" for error in group_data.get('errors', [])):
-    print(f"\x1b[31mToo Many Requests > https://www.roblox.com/groups/{ID}")
+      print(f"\x1b[31mClaimed | https://www.roblox.com/groups/{ID} ({group_data['0memberCount']})")
+  elif any(error.get("message") == "Too many requests" for error in group_data.get("errors", [])):
+    print(f"\x1b[31mToo Many Requests | https://www.roblox.com/groups/{ID}")
   else:
-    print(f"\x1b[31mInvalid > https://www.roblox.com/groups/{ID}")
+    print(f"\x1b[31mInvalid | https://www.roblox.com/groups/{ID}")
