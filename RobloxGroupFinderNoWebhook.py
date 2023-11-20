c2897b0f050cb18bdba1920012fa714cd621c629
SquidDog <rodrigobatalloso@proton.me>
2023-11-20 21:57:23 +0100

    Update RobloxGroupFinderNoWebhook.py

--- RobloxGroupFinderNoWebhook.py
+++ RobloxGroupFinderNoWebhook.py
@@ -3,7 +3,7 @@ import random
 import requests
 while True:
   ID = random.randint(9450000, 10000000)
-  r = requests.get(f'https://groups.roproxy.com/v1/groups/{ID}')
+  r = requests.get(f"https://groups.roproxy.com/v1/groups/{ID}")
   try:
     group_data = r.json()
   except json.JSONDecodeError as e:
@@ -11,13 +11,13 @@ while True:
     continue 
   if "owner" in r.text:
     if group_data['owner'] is None: 
-      if group_data['publicEntryAllowed'] is True and 'isLocked' not in r.text:
-        print(f"\x1b[42mUnclaimed > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
+      if group_data['publicEntryAllowed'] is True and "isLocked" not in r.text:
+        print(f"\x1b[32mUnclaimed | https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
       else:
-        print(f"\x1b[31mLocked > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
+        print(f"\x1b[33mLocked | https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
     else:
-      print(f"\x1b[31mClaimed > https://www.roblox.com/groups/{ID} ({group_data['memberCount']})")
-  elif any(error.get('message') == "Too many requests" for error in group_data.get('errors', [])):
-    print(f"\x1b[31mToo Many Requests > https://www.roblox.com/groups/{ID}")
+      print(f"\x1b[31mClaimed | https://www.roblox.com/groups/{ID} ({group_data['0memberCount']})")
+  elif any(error.get("message") == "Too many requests" for error in group_data.get("errors", [])):
+    print(f"\x1b[31mToo Many Requests | https://www.roblox.com/groups/{ID}")
   else:
-    print(f"\x1b[31mInvalid > https://www.roblox.com/groups/{ID}")
+    print(f"\x1b[31mInvalid | https://www.roblox.com/groups/{ID}")
